e96176257220092a14244d28da9762210f741fdc
Bjorn Chinnapongse <115442509+chocwaffle@users.noreply.github.com>
2025-01-13 02:26:58 +0800

    Added Files

--- Engine.hpp
+++ Engine.hpp
@@ -39,27 +39,27 @@ namespace btEngine
         void shutdown(entt::registry* registry);
 
         window::windowSystem&                   getWindow()             const { return *mWindow;        }
-		TeaPhysics::physicsSystem&              getPhysics()            const { return *physicsSystem;  }
-		audio::audioSystem&                     getAudio()              const { return *audioSystem;    }
+	TeaPhysics::physicsSystem&              getPhysics()            const { return *physicsSystem;  }
+	audio::audioSystem&                     getAudio()              const { return *audioSystem;    }
         TeaGraphics::graphicsSystem&            getGraphics()           const { return *graphicsSystem; }
         TeaParticles::particleSystem&           getParticles()          const { return *particleSystem; }
-		profiler::ProfilerSystem&               getProfiler()           const { return *mProfiler;      }
+	profiler::ProfilerSystem&               getProfiler()           const { return *mProfiler;      }
         SceneManager::sceneManager&             getSceneManager()             { return *sceneManager;   }
-		Scripting::ScriptCore&                  getScriptCore()         const { return *mScriptCore;    }
-		Scripting::ScriptRuntime&               getScriptRuntime()      const { return *mScriptRuntime; }
-		TeaAnimation::Animator&                 getAnimator()           const { return *mAnimator;      }
+	Scripting::ScriptCore&                  getScriptCore()         const { return *mScriptCore;    }
+	Scripting::ScriptRuntime&               getScriptRuntime()      const { return *mScriptRuntime; }
+	TeaAnimation::Animator&                 getAnimator()           const { return *mAnimator;      }
 
     private:
 
         std::unique_ptr<window::windowSystem>                   mWindow                 = std::make_unique<window::windowSystem>();
         std::unique_ptr<TeaPhysics::physicsSystem>              physicsSystem           = std::make_unique<TeaPhysics::physicsSystem>();
         std::unique_ptr<audio::audioSystem>                     audioSystem             = std::make_unique<audio::audioSystem>();
-		std::unique_ptr<SceneManager::sceneManager>             sceneManager            = std::make_unique<SceneManager::sceneManager>();
+	std::unique_ptr<SceneManager::sceneManager>     	sceneManager            = std::make_unique<SceneManager::sceneManager>();
         std::unique_ptr<TeaParticles::particleSystem>           particleSystem          = std::make_unique<TeaParticles::particleSystem>();
         std::unique_ptr<TeaGraphics::graphicsSystem>            graphicsSystem          = std::make_unique<TeaGraphics::graphicsSystem>();
-		std::unique_ptr<profiler::ProfilerSystem>               mProfiler               = std::make_unique<profiler::ProfilerSystem>();
-		std::unique_ptr<Scripting::ScriptCore>                  mScriptCore             = std::make_unique<Scripting::ScriptCore>();
-		std::unique_ptr<Scripting::ScriptRuntime>               mScriptRuntime          = std::make_unique<Scripting::ScriptRuntime>();
-		std::unique_ptr<TeaAnimation::Animator>                 mAnimator               = std::make_unique<TeaAnimation::Animator>();
+	std::unique_ptr<profiler::ProfilerSystem>               mProfiler               = std::make_unique<profiler::ProfilerSystem>();
+	std::unique_ptr<Scripting::ScriptCore>                  mScriptCore             = std::make_unique<Scripting::ScriptCore>();
+	std::unique_ptr<Scripting::ScriptRuntime>               mScriptRuntime          = std::make_unique<Scripting::ScriptRuntime>();
+	std::unique_ptr<TeaAnimation::Animator>                 mAnimator               = std::make_unique<TeaAnimation::Animator>();
     };
 }
